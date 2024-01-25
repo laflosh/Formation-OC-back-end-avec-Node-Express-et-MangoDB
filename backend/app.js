2a9f9109c33e198b600d64caeca0b7ef1ece814c
laflosh <l_florent_l@hotmail.fr>
2024-01-25 12:19:13 +0100

    Découverte middleware

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,24 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.use((req, res) => {
-    res.json({message:"votre requte a bien été reçu"})
+app.use((req, res, next) =>{
+    console.log("requete recue");
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next) => {
+    res.json({message:"votre requte a bien été reçu"});
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("response envoyé avec succès")
 })
 
 module.exports = app;
