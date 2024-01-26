602b828dc9a495b2718c7b51c43c726a88fa276a
laflosh <l_florent_l@hotmail.fr>
2024-01-26 10:46:24 +0100

    Première route GET et fix pb CORS

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) =>{
-    console.log("requete recue");
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTION");
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-})
+app.use("/api/stuff",(req, res, next) => {
+    const stuff = [
+        {
+            _id: "skhdbfisdbf",
+            title: "Mon premier objet",
+            description: "Les infos de mon première objet",
+            imageUrl: "",
+            price: 4900,
+            userId: "euhfsfhe"
+        },
+        {
+            _id: "fghisudhgf",
+            title: "Mon deuxième objet",
+            description: "Les infos de mon deuxième objet",
+            imageUrl: "",
+            price: 2900,
+            userId: "fjsppezj"
+        },
+    ];
 
-app.use((req, res, next) => {
-    res.json({message:"votre requte a bien été reçu"});
-    next();
+    res.status(200).json(stuff);
 });
-
-app.use((req, res, next) => {
-    console.log("response envoyé avec succès")
-})
 
 module.exports = app;
